c492b0264e5d633df3439276608529488b839c44
tushal012 <tushalkashyap11@gmail.com>
2021-05-07 20:08:01 +0530

    calculating wages for month

---
+++ empWageV3.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -x
+
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
+
+   for (( day=1; day<=$numWorkingDays; Day++ ))
+	do
+		empCheck=$((RANDOM%3));
+		case $empCheck in
+		 $isFullTime)
+			empHrs=4
+			;;
+			$isPartTime)
+				empHrs=4
+				;;
+				*)
+			empHrs=0;;
+		esac
+		salary=$(($empHrs*$empRatePerHr));
+		totalSalary=$(($totalSalary+$salary));
+	done
