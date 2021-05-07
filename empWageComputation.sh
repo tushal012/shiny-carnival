ca5ee54914b59e5f893601ff37b87821c0de3485
tushal012 <tushalkashyap11@gmail.com>
2021-05-07 18:04:17 +0530

    added employee is absent or present

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,17 @@
 #! /bin/bash -x
 
 echo "Welcome to EmployeeWage"
+
+IS_PRESENT=1
+
+#Variables
+empCheck=$(( RANDOM % 2  ))
+
+#Selection
+
+if [$empcheck -eq $IS_PRESENT ]
+then
+	echo "employee is  present"
+else
+	echo "employee is absent"
+fi
